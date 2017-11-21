f7b0738ffbf247f30655d62cdd53ba151242152e
K9Grillz <33672838+K9Grillz@users.noreply.github.com>
2017-11-21 11:20:53 -0500

    fixed

--- Project/src/coe528/project/CanadaPost.java
+++ Project/src/coe528/project/CanadaPost.java
@@ -4,11 +4,7 @@ public class CanadaPost extends Shipper {
 
     protected static int ExpShip = 120;
 
-    protected CanadaPost() {
-        this("Test","Sample");
-    }
-
-    protected CanadaPost(String name, String location) {
+    public CanadaPost(String name, String location) {
         super(name, location, ExpShip);
     }
 
