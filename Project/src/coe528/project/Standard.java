84aaae7358e7acf3cd84b9fc62aab5b17151fc99
K9Grillz <33672838+K9Grillz@users.noreply.github.com>
2017-11-21 11:24:31 -0500

    not sure what changed

--- Project/src/coe528/project/Standard.java
+++ Project/src/coe528/project/Standard.java
@@ -3,19 +3,20 @@ package coe528.project;
 public class Standard extends Priority {
     //Allows canceling the order before it is shipped.
 
-
     public Standard() {
-      super();
-      super.canc = true;
+        super();
+        super.canc = true;
     }
 
     @Override
-    //Make sure item is not shipped otherwise we can't return
     protected boolean cancel() {
-        if(!this.shipped) return true;
+        if (!this.shipped) {
+            return true;
+        }
         return false;
     }
 
+    @Override
     protected void setShipper(Shipper ship) {
         super.setShipper(ship);
         this.canc = false;
